03fec447dd700f0d0c1b0e41de84fafc79f80d72
nikolairoshchin <99800316+nikolairoshchin@users.noreply.github.com>
2023-01-25 15:27:18 +0200

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const brackets = [];
+  for (let i=0; i<str.length; i++) {
+    for (let bracket of bracketsConfig) {
+      switch (str[i]) {
+        case bracket[1]:
+          if (bracket[0] === brackets.at(-1)) { 
+            brackets.pop();
+            break;
+          };
+        case bracket[0]:
+          brackets.push(bracket[0]);
+          break;
+      };
+    };
+  };
+  if (brackets.length === 0) { return true; };
+  return false;
 }
